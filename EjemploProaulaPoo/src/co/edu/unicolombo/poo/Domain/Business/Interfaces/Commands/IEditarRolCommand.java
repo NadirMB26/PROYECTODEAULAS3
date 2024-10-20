9a60deac9266f275e2876455623cd0282d17effb
Nadir Marsiglia <159026044+NadirMB26@users.noreply.github.com>
2024-10-19 22:57:47 -0500

    Delete EjemploProaulaPoo directory

--- EjemploProaulaPoo/src/co/edu/unicolombo/poo/Domain/Business/Interfaces/Commands/IEditarRolCommand.java
+++
@@ -1,9 +0,0 @@
-
-package co.edu.unicolombo.poo.Domain.Business.Interfaces.Commands;
-
-import co.edu.unicolombo.poo.Domain.Business.Interfaces.Usecases.EditarRolCommand;
-
-
-public interface IEditarRolCommand {
- public void editar (EditarRolCommand comand)throws Exception;
-}
--- EjemploProaulaPoo/src/co/edu/unicolombo/poo/Domain/Business/Interfaces/Commands/IGuardarRoleCommand.java
+++
@@ -1,8 +0,0 @@
-
-package co.edu.unicolombo.poo.Domain.Business.Interfaces.Commands;
-
-import co.edu.unicolombo.poo.Domain.Business.Interfaces.Usecases.GuardarRolCommand;
-
-public interface IGuardarRoleCommand {
-  public int createRole(GuardarRolCommand comando)throws Exception;  
-}
